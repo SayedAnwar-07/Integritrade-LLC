1ff475902d98a8ebab229b3445cab46c182ad47b
SayedAnwarOfficial <sayedanwarofficial@gmail.com>
2026-07-12 11:21:26 +0600

    change toggle system

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="light"
-            enableSystem
+            enableSystem={false}
             disableTransitionOnChange
           >
             <Suspense fallback={null}>
