7d07ea95983eeeedd5abd984e336dfd65a18ea21
SayedAnwarOfficial <sayedanwarofficial@gmail.com>
2026-09-01 11:02:36 +0600

    Update ISO certification images and assets

--- data/certificates.ts
+++ data/certificates.ts
@@ -3,14 +3,14 @@ export const CERTIFICATE_PDFS = {
     "/certificates/Integritrade%20LLC%20R2v3%20Electronics%20Recycling%20Data%20Security%20and%20ITAD.pdf",
 
   iso9001:
-    "/certificates/Integritrade%20LLC%20ISO%209001%20Electronics%20Recycling%20and%20ITAD%20QMS.pdf",
+    "/certificates/Integritrade_ISO_9001_QMS_Certification_2026.pdf",
 
   iso14001:
-    "/certificates/Integritrade%20LLC%20ISO%2014001%20Electronics%20Recycling%20and%20ITAD%20EMS.pdf",
+    "/certificates/Integritrade_ISO_14001_EHS_Certification_2026.pdf",
 
   iso27001:
     "/certificates/Integritrade%20LLC%20ISO%20IEC%2027001%20Electronics%20Recycling%20and%20ITAD%20ISMS.pdf",
 
   iso45001:
-    "/certificates/Integritrade%20LLC%20ISO%2045001%20Electronics%20Recycling%20and%20ITAD%20OHS.pdf",
+    "/certificates/Integritrade_ISO_45001_Certification_2026.pdf",
 } as const;
